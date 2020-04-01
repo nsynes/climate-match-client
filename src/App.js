e36124ad5a64ebd190bd82f5463bc0d6bd33db4a
nsynes <nsynes@gmail.com>
2020-04-01 16:40:48 +0100

    Added promise polyfill for IE11 support

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Inputs from './components/Inputs';
 import Loading from './components/Loading';
 import { API_URL_ClimateMatch } from './config';
 import{ handleResponse } from './helpers';
+require('es6-promise').polyfill();
 
 
 class App extends React.Component {

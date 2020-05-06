af925e43807e8bb82a7e0408c3bf2358b576c370
nsynes <nsynes@gmail.com>
2020-05-06 13:17:57 +0100

    Addedfix for IE overflow limitation

--- src/components/PanelContentBody.js
+++ src/components/PanelContentBody.js
@@ -3,8 +3,12 @@ import './PanelContentBody.css';
 
 const PanelContentBody = (props) => {
 
+    // Rather imperfect fix for IE requirement for height to be set for overflow:auto element
+    const isIE = /*@cc_on!@*/false || !!document.documentMode;
+    const ieStyle = isIE ? {maxHeight: `${window.innerHeight- 180}px`} : {};
+
     return (
-        <div className='panel-content-body'>
+        <div className='panel-content-body' style={ieStyle}>
             {props.children}
         </div>
     );
